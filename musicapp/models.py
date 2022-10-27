31fda6d315f4fde38eab2c22310a3973e9fc9eed
hanif5649 <114647292+hanif5649@users.noreply.github.com>
2022-10-27 23:56:31 +0100

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
-# Create your models here.
+Model:  
+    Artiste: Polo G.
+    Song: Through Da Storm. 
+    Lyric: Hey big brother, it's me, Leah
+Remember at the old house I said you was gonna be a big star one day?
+I'm so proud of you
+Mmh, mmh, mmh, Polo G
+Live in the flesh
+
+Know my grandma still with me, when it get cold, I feel your spirit
+Talkin' to my lil' sister, phone calls through Securus
+Walk in court in them shackles, see my mama, her eyes tearin'
+Tryna work towards these blessings but the devil keep interfering
+Everybody go through something, it's all about perseverance
+They was counting me out, I put passion in every lyric
+Fuck a sack up at Neimans, spend some racks on my appearance
+Yeah, I know that they hate, I'm the man, ain't tryna hear it
+
+Phone on DND, I'm out in Cali whippin' through the mountains
+Just cruisin', feelin' my song, windows down, speakers pounding, yeah
+My engine roaring and that Gucci tiger growling
+Expensive taste, my clothes foreign, I be dripping like a fountain
+Need new shoes, I started hustling 'cause I didn't get no allowance
+Speeding down the wrong path, found my lane, then I rerouted
+Hottest in my city, I heard every word the labels scouted
+I got rich and left the hood, still don't leave the crib without it
+Swear it ain't no better feeling than thumbing through these thousands
+So much money, can't keep track, I think I might need an accountant
+I been runnin' up that check, them blue hundreds just keep piling
+Came a long way from depression, all these riches keep me smiling
+
+Know my grandma still with me, when it get cold, I feel your spirit
+Talkin' to my lil' sister, phone calls through Securus
+Walk in court in them shackles, see my mama, her eyes tearin'
+Tryna work towards these blessings but the devil keep interfering
+Everybody go through something, it's all about perseverance
+They was counting me out, I put passion in every lyric
+Fuck a sack up at Neimans, spend some racks on my appearance
+Yeah, I know that they hate, I'm the man, ain't tryna hear it
+
+Couldn't leave my brother in them
+Trenches, told him come and stay with me
+We gon' live like kings for all
+Them nights ain't have no place to sleep
+Niggas watched us starving, never offered us a place to eat
+Took off, now they mad, but I know that they won't wait for me
+Last year I took a trip to every place that I couldn't wait to see
+Granny, I'm locked up again, sat in that station faithfully
+Now I'm the golden child, I used to think my mama hated me
+Ain't budging 'bout no problems,
+Swear these niggas ain't gon' take from me
+I heard you a robber, on that concrete, you gon' lay deceased
+This .40 waving like a blowdryer, gon' hit his face with heat
+With this shit for live, forever gang until I'm 83
+Real niggas dyin', it seem like Heaven is the place to be
+
+Know my grandma still with me, when it get cold, I feel your spirit
+Talkin' to my lil' sister, phone calls through Securus
+Walk in court in them shackles, see my mama, her eyes tearin'
+Tryna work towards these blessings but the devil keep interfering
+Everybody go through something, it's all about perseverance
+They was counting me out, I put passion in every lyric
+Fuck a sack up at Neimans, spend some racks on my appearance
+Yeah, I know that they hate, I'm the man, ain't tryna hear it
+
+
+
+Attributes for “Artiste” : 
+   POLO G, 23
+    
+Attributes for “Song” : 
+   Title: Through Da Storm. 
+   Date Released: June 7, 2019.
+   Likes: 341k likes
+   Artiste_id: https://open.spotify.com/track/3NqCEpT8xsHdzpiRlM1kpS?si=2b314e928c104571&nd=1
+      
+Attributes for “Lyric”: 
+   https://www.google.com/search?q=through+da+storm+lyrics&oq=through+da+stor&aqs=chrome.3.0i355i512j46i512j69i57j0i512l3j69i61l2.11795j0j7&sourceid=chrome&ie=UTF-8
